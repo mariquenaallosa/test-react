30393096afbbad9d4666a79b5452384022996dfa
mariquenaallosa <allosamariquena@gmail.com>
2025-03-05 16:27:44 -0300

    feat: add initial project setup with React and Vite, including configuration files, styles, and basic component structure.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold text-[#1A2B49] mb-8">Hotel Listings</h1>
+       
+      </div>
+    </div>
+  )
+}
+
+export default App
