503ccb8edfe4e3a5524c7a4e38b930bc79fb105b
mariquenaallosa <allosamariquena@gmail.com>
2025-03-05 18:18:21 -0300

    feat: add ImageCarousel component for displaying hotel images

---
+++ src/components/ImageCarousel.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const ImageCarousel = ({ images }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
+  return (
+    <Slider {...settings}>
+      {images.map((image, index) => (
+        <div key={index}>
+          <img src={image} alt={`Hotel image ${index + 1}`} className="w-full h-48 md:h-64 object-cover" />
+        </div>
+      ))}
+    </Slider>
+  );
+};
+
+export default ImageCarousel;
