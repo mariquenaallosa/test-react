dd61c9bbf95712a2b5284f9bf6b8b4e1c944d4cc
mariquenaallosa <allosamariquena@gmail.com>
2025-03-05 18:17:30 -0300

    feat: add Spinner component and API service for hotel data fetching

---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const urlApi = "https://wmw3lg8sha.execute-api.us-east-2.amazonaws.com/dev/dummy";
+
+export const fetchHotels = async () => {
+    try {
+        const response = await axios.get(urlApi);
+
+        return response.data.data;
+        
+
+    } catch (error) {
+        console.error("Error fetching hotels", error);
+        throw error;
+    }
+}
